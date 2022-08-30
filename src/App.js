58e6cf3875601119dab941a6e9894ef3e2810c52
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-30 17:50:07 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from './contacts.json';
+import { useState } from 'react';
+const firstFive = contacts.slice(0, 5);
 
 function App() {
+  const [celebrity, setCelebrity] = useState(firstFive);
+  const celebrityList = [...celebrity];
+
+  const addRandom = () => {
+    let i = Math.floor(Math.random() * contacts.length);
+    if (!celebrityList.includes(contacts[i])){
+      celebrityList.push(contacts[i]);
+    }
+    setCelebrity(celebrityList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <h1>IronContacts</h1>
+    <button onClick={() => addRandom()}>Add Random Contact</button> 
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        {celebrityList.map(celebs => 
+          <tr>
+            <td><img src={celebs.pictureUrl} alt='celebPicture'/></td>
+            <td>{celebs.name}</td>
+            <td>{Math.round(celebs.popularity)}</td>
+            <td>{celebs.wonOscar ? '🏆' : ""}</td>
+            <td>{celebs.wonEmmy ? '🏆' : ""}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  </div>  
   );
 }
 
